f2059a29c5c7ca45dbf5a9af646a4dda2c234337
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-02-23 17:41:31 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sofia_shopping_list
 # sofia_shoppinglist-
+# sofia_shoppinglist-
+# sofia_shopping_list
--- index.js
+++ index.js
@@ -1,109 +1,40 @@
-'use strict';
+function addListItem() {
+    
+    var write = $('#newItem').val();
+    var list = $('#itemList');
+    var item = $('<li><button class="checkbox">&#x2713;</button><span class="list">' + write + '</span><button class="delete">X</button></li>');
+    var newItem = $("#newItem");
 
-const STORE = [
-  {name: "apples", checked: false},
-  {name: "oranges", checked: false},
-  {name: "milk", checked: true},
-  {name: "bread", checked: false}
-];
-
-
-function generateItemElement(item, itemIndex, template) {
-  return `
-    <li class="js-item-index-element" data-item-index="${itemIndex}">
-      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
-      <div class="shopping-item-controls">
-        <button class="shopping-item-toggle js-item-toggle">
-            <span class="button-label">check</span>
-        </button>
-        <button class="shopping-item-delete js-item-delete">
-            <span class="button-label">delete</span>
-        </button>
-      </div>
-    </li>`;
+    if (write.length === 0  || write.length > 33) {
+       return false;
+   }
+   
+   list.append(item);
+   $(newItem).val('');
 }
 
+function deleteItem() {
 
-function generateShoppingItemsString(shoppingList) {
-  console.log("Generating shopping list element");
-
-  const items = shoppingList.map((item, index) => generateItemElement(item, index));
-  
-  return items.join("");
+   $(this).parent().remove();
 }
 
+function dimItem() {
 
-function renderShoppingList() {
-  
-  console.log('`renderShoppingList` ran');
-  const shoppingListItemsString = generateShoppingItemsString(STORE);
-
-  
-  $('.js-shopping-list').html(shoppingListItemsString);
+   $(this).parent().toggleClass('dim');
 }
 
+$(function() {
 
-function addItemToShoppingList(itemName) {
-  console.log(`Adding "${itemName}" to shopping list`);
-  STORE.push({name: itemName, checked: false});
-}
-
-function handleNewItemSubmit() {
-  $('#js-shopping-list-form').submit(function(event) {
-    event.preventDefault();
-    console.log('`handleNewItemSubmit` ran');
-    const newItemName = $('.js-shopping-list-entry').val();
-    $('.js-shopping-list-entry').val('');
-    addItemToShoppingList(newItemName);
-    renderShoppingList();
-  });
-}
-
-function toggleCheckedForListItem(itemIndex) {
-  console.log("Toggling checked property for item at index " + itemIndex);
-  STORE[itemIndex].checked = !STORE[itemIndex].checked;
-}
-
-
-function getItemIndexFromElement(item) {
-  const itemIndexString = $(item)
-    .closest('.js-item-index-element')
-    .attr('data-item-index');
-  return parseInt(itemIndexString, 10);
-}
-
-function handleItemCheckClicked() {
-  $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
-    console.log('`handleItemCheckClicked` ran');
-    const itemIndex = getItemIndexFromElement(event.currentTarget);
-    toggleCheckedForListItem(itemIndex);
-    renderShoppingList();
-  });
-}
-
-
-function deleteListItem(itemIndex) {
-  console.log(`Deleting item at index  ${itemIndex} from shopping list`)
-
-  
-  STORE.splice(itemIndex, 1);
-}
-
-
-function handleDeleteItemClicked() {
-  $('.js-shopping-list').on('click', '.js-item-delete', event => {
-    const itemIndex = getItemIndexFromElement(event.currentTarget);
-    deleteListItem(itemIndex);
-    renderShoppingList();
-  });
-}
-
-
-function handleShoppingList() {
-  renderShoppingList();
-  handleNewItemSubmit();
-  handleItemCheckClicked();
-  handleDeleteItemClicked();
-}
-
-$(handleShoppingList);
+   var add = $('#addItem');
+   var newItem = $('#newItem');
+   var list = $('#itemList');
+   
+   add.on('click', addListItem);
+   list.on('click', '.checkbox', dimItem);
+   list.on('click', '.delete', deleteItem);
+   newItem.on('keypress', function (e) {
+       if (e.which == 13) {
+           addListItem();
+       }
+   });
+});
